b12521d694e8e6126789f2aa51c94eedfa0032d0
marcos <LM!@#brz9>
2019-06-05 22:25:29 -0300

    commit inicial layout

---
+++ pages/index.js
@@ -0,0 +1,160 @@
+import 'antd/dist/antd.css'
+import { Component } from 'react';
+import { Layout, Menu, Icon, Button } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+
+export default class Index extends Component{
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render(){
+    return(
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0,
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: 200 }}>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Button type="primary"
+                    onClick={this.toggle}
+                    shape="circle"
+                    style={{marginLeft: '30px'}}
+                    icon={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+
+          </Header>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+              ...
+              <br />
+              Really
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              long
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
