c7fde7b4fb09818e3f67ff510f97c7330dcffb95
marcos <LM!@#brz9>
2019-06-06 23:29:47 -0300

    login

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withLess = require('@zeit/next-less')
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withLess = require('@zeit/next-less');
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
@@ -12,5 +12,8 @@ if (typeof require !== 'undefined') {
 module.exports = withLess(withSass(withCSS({
   lessLoaderOptions: {
     javascriptEnabled: true
+  },
+  cssLoaderOptions: {
+    url: true
   }
 })));
--- pages/index.js
+++ pages/index.js
@@ -1,160 +1,128 @@
-import 'antd/dist/antd.css'
-import { Component } from 'react';
-import { Layout, Menu, Icon, Button } from 'antd';
+import React from 'react';
+import Link from 'next/link.js';
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
 
-const { Header, Content, Footer, Sider } = Layout;
+import 'antd/dist/antd.css';
+import { Form, Icon, Input, Button, Checkbox, Row, Col, Spin, Typography } from 'antd';
+
+const serverUrl = 'http://cipa.velow.com.br';
 
 
 
-export default class Index extends Component{
-  state = {
-    collapsed: false,
-  };
+// set up cookies
+const cookies = new Cookies();
+const { Title } = Typography;
+class Index extends React.Component {
 
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: cookies.get('token') || null,
+      user: null,
+      password: null,
+      loading: false
+    }
+
+
+  }
+
+
+  loginClick = async (e) => {
+
+    e.preventDefault();
+
+    let body = {}
+    this.state.loading = true;
+    this.props.form.validateFields((err, obj) => {
+      if (!err) {
+        console.log(obj)
+        body.email = obj.email;
+        body.password = obj.password;
+      }
     });
+    debugger;
+
+    const response = await axios.post(serverUrl + '/auth/sign_in',  body );
+    cookies.set('token', response.headers["access-token"]);
+    this.setState({
+      token: response.headers["access-token"]
+    })
   };
 
   render(){
-    return(
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}
-          style={{
-            overflow: 'auto',
-            height: '100vh',
-            position: 'fixed',
-            left: 0,
-          }}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ marginLeft: 200 }}>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <Button type="primary"
-                    onClick={this.toggle}
-                    shape="circle"
-                    style={{marginLeft: '30px'}}
-                    icon={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
 
-          </Header>
-          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-            <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
-              ...
-              <br />
-              Really
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              long
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              content
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
-      </Layout>
+    const divLogin = {
+      height: '100%',
+      position: 'absolute',
+      width: '100%'
+    };
+
+    const loginBackground = {
+      background: "url('/static/images/login_background.png') no-repeat center",
+      height: '100%',
+      objectFit: 'cover'
+    };
+
+    return (
+
+      <Row type="flex" style={divLogin}>
+        <Col style={loginBackground} span={12}>
+        </Col>
+        <Col span={12}>
+          <Row type="flex" style={{height: '100%', marginRight: '20%'}} align="middle" justify="end">
+            <Col span={15}>
+
+              {!this.state.loading && (
+                <Title>Velow CIPA</Title>
+              )}
+
+              {this.state.loading && (
+                <Row type="flex" align="middle" justify="center">
+                  <Spin size="large" className="login-form"
+                        tip="Carregando..."></Spin>
+                </Row>
+              )}
+
+              {!this.state.loading && (
+
+                <Form onSubmit={this.loginClick} className="login-form">
+                  <Form.Item>
+                    {getFieldDecorator('email', {
+                      rules: [{ required: true, message: 'Insira o seu usuário ou e-mail!' }],
+                    })(
+                      <Input
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="E-mail"
+                      />,
+                    )}
+                  </Form.Item>
+                  <Form.Item>
+                    {getFieldDecorator('password', {
+                      rules: [{ required: true, message: 'Insira a sua senha!' }],
+                    })(
+                      <Input
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        type="password"
+                        placeholder="Senha"
+                      />,
+                    )}
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                      Entrar
+                    </Button>
+                  </Form.Item>
+                </Form>
+              )}
+            </Col>
+          </Row>
+
+        </Col>
+      </Row>
+
     );
   }
 }
+export default Form.create()(Index);
