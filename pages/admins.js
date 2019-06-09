9160eca60d98525bab73a9a2ad4d45bfad8836e8
marcos <LM!@#brz9>
2019-06-09 00:17:37 -0300

    Convertido projeto CIPA para projeto base

--- pages/admins.js
+++ pages/admins.js
@@ -3,32 +3,32 @@ import axios from 'axios';
 import { Cookies } from 'react-cookie';
 import { handleAuthSSR } from '../utils/auth';
 
+import {PageLoader} from '../components/PageLoader';
+
 const serverUrl = 'http://cipa.velow.com.br';
 
 // set up cookies
 const cookies = new Cookies();
 
-class Secret extends React.Component {
+class Admins extends React.Component {
 
-  onPingCall = async (e) => {
-    const token = cookies.get('token');
-
-    try {
-      const res = await axios.get(serverUrl + '/api/ping', { headers: { 'Authorization': token } });
-      console.log(res.data.msg);
-    } catch (err) {
-      console.log(err.response.data.msg);
-    }
+  constructor(){
+    super();
+    this.state = {pageLoading: true}
   }
+
+  componentDidMount(){
+    this.setState({pageLoading: false});
+  }
+
 
   render() {
     return (
-      <div>
-        <h2>Secret page</h2>
-        <p>Only accessible via a valid JWT</p>
-        <br></br>
-        <button onClick={(e) => this.onPingCall(e)}>Ping Call</button>
-        <p>Check console for response</p>
+      <div id="main">
+        {this.state.pageLoading ? <PageLoader/> : ""}
+        <div>
+          Login realizado com sucesso
+        </div>
       </div>
     );
   }
@@ -46,4 +46,4 @@ class Secret extends React.Component {
 //   return {}
 // }
 
-export default Secret;
+export default Admins;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+//TODO animações, melhora no submit de formulario (dados em branco) e tradução
+
 import React from 'react';
-import Link from 'next/link.js';
 import axios from 'axios';
 import { Cookies } from 'react-cookie';
-
+import Swal from 'sweetalert2'
+import Router from 'next/router';
 import 'antd/dist/antd.css';
+import {PageLoader} from '../components/PageLoader';
 import { Form, Icon, Input, Button, Checkbox, Row, Col, Spin, Typography } from 'antd';
 
 const serverUrl = 'http://cipa.velow.com.br';
@@ -21,34 +24,59 @@ class Index extends React.Component {
       token: cookies.get('token') || null,
       user: null,
       password: null,
-      loading: false
-    }
+      loading: false,
+      pageLoading: true
+    };
+
+    this.emmitToastMessage = (type, messages) => {
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-start',
+        showConfirmButton: false,
+        timer: 3000
+      });
+
+      Toast.fire({
+        type: type,
+        title: messages.join('\n *')
+      })
+    };
 
 
   }
 
-
   loginClick = async (e) => {
-
     e.preventDefault();
 
-    let body = {}
+    let body = {};
     this.state.loading = true;
     this.props.form.validateFields((err, obj) => {
       if (!err) {
-        console.log(obj)
         body.email = obj.email;
         body.password = obj.password;
       }
     });
-    debugger;
 
-    const response = await axios.post(serverUrl + '/auth/sign_in',  body );
-    cookies.set('token', response.headers["access-token"]);
-    this.setState({
-      token: response.headers["access-token"]
-    })
+    await axios.post(serverUrl + '/auth/sign_in',  body).then((resp) => {
+      cookies.set('token', resp.headers["access-token"]);
+      this.setState({
+        token: resp.headers["access-token"]
+      });
+
+      this.setState({loading: false});
+      // this.emmitToastMessage('success', ['Usuário logado com sucesso']);
+      Router.push('/admins')
+    }).catch((reject) => {
+      this.setState({loading: false});
+      this.emmitToastMessage('error', reject.response.data.errors);
+    });
+
   };
+
+  componentDidMount(){
+    this.setState({pageLoading: false});
+  }
 
   render(){
     const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
@@ -66,63 +94,63 @@ class Index extends React.Component {
     };
 
     return (
+      <div id="main">
+        {this.state.pageLoading ? <PageLoader/> : ""}
 
-      <Row type="flex" style={divLogin}>
-        <Col style={loginBackground} span={12}>
-        </Col>
-        <Col span={12}>
-          <Row type="flex" style={{height: '100%', marginRight: '20%'}} align="middle" justify="end">
-            <Col span={15}>
+        <Row type="flex" style={divLogin}>
+          <Col style={loginBackground} span={12}>
+          </Col>
+          <Col span={12}>
+            <Row type="flex" style={{height: '100%', marginRight: '20%'}} align="middle" justify="end">
+              <Col span={15}>
 
-              {!this.state.loading && (
-                <Title>CIPA</Title>
-              )}
+                {!this.state.loading && (
+                  <Title>TITLE LOREM IPSUM</Title>
+                )}
 
-              {this.state.loading && (
-                <Row type="flex" align="middle" justify="center">
-                  <Spin size="large" className="login-form"
-                        tip="Carregando..."></Spin>
-                </Row>
-              )}
+                {this.state.loading && (
+                  <Row type="flex" align="middle" justify="center">
+                    <Spin size="large" className="login-form"
+                          tip="Carregando..."></Spin>
+                  </Row>
+                )}
 
-              {!this.state.loading && (
+                {!this.state.loading && (
+                  <Form onSubmit={this.loginClick} className="login-form">
+                    <Form.Item>
+                      {getFieldDecorator('email', {
+                        rules: [{ required: true, message: 'Insira o seu usuário ou e-mail!' }],
+                      })(
+                        <Input
+                          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          placeholder="E-mail"
+                        />,
+                      )}
+                    </Form.Item>
+                    <Form.Item>
+                      {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Insira a sua senha!' }],
+                      })(
+                        <Input
+                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          type="password"
+                          placeholder="Senha"
+                        />,
+                      )}
+                    </Form.Item>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" className="login-form-button">
+                        Entrar
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                )}
+              </Col>
+            </Row>
 
-                <Form onSubmit={this.loginClick} className="login-form">
-                  <Form.Item>
-                    {getFieldDecorator('email', {
-                      rules: [{ required: true, message: 'Insira o seu usuário ou e-mail!' }],
-                    })(
-                      <Input
-                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        placeholder="E-mail"
-                      />,
-                    )}
-                  </Form.Item>
-                  <Form.Item>
-                    {getFieldDecorator('password', {
-                      rules: [{ required: true, message: 'Insira a sua senha!' }],
-                    })(
-                      <Input
-                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        type="password"
-                        placeholder="Senha"
-                      />,
-                    )}
-                  </Form.Item>
-                  <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                      Entrar
-                    </Button>
-                  </Form.Item>
-                </Form>
-              )}
-            </Col>
-          </Row>
-
-        </Col>
-      </Row>
-
-    );
+          </Col>
+        </Row>
+      </div>    );
   }
 }
 export default Form.create()(Index);
