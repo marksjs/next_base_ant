0ef1f0952d1ed37c75d6748a13e9b5ec7c9a41f4
marcos <LM!@#brz9>
2019-07-21 22:56:43 -0300

    Improved responsible of sidenav and add some actions buttons

--- components/Menu/Sidenav.js
+++ components/Menu/Sidenav.js
@@ -1,33 +1,35 @@
 import React, { Component, useEffect, useState } from 'react';
-import { Layout, Menu, Icon, Button, Divider } from 'antd';
+import { Row, Col, Layout, Menu, Icon, Button, Divider } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Content,  Footer, Sider } = Layout;
 
 export class Sidenav extends Component {
-  state = {
-    theme: 'dark',
-    current: '1',
-    collapsed: false
-  };
+
+  constructor(){
+    super();
+    this.state = {
+      theme: 'dark',
+      current: '1',
+      collapsed: true,
+      clientSide: false
+    };
+  }
 
   updateDimensions = async () => {
     this.setState({width: window.innerWidth, height: window.innerHeight});
   };
 
-  componentWillMount() {
-    this.updateDimensions();
-  };
-
   componentDidMount() {
+    this.updateDimensions();
     window.addEventListener("resize", this.updateDimensions);
   };
 
   componentWillUnmount() {
+    this.updateDimensions();
     window.removeEventListener("resize", this.updateDimensions);
   };
 
   onCollapse = collapsed => {
-    console.log(collapsed);
     this.setState({ collapsed });
   };
 
@@ -46,10 +48,32 @@ export class Sidenav extends Component {
 
   render() {
     return (
-      <Sider className="sider" theme={this.state.theme} collapsed={this.state.width <= 1012 ? true : false} onCollapse={this.onCollapse}>
+      <Sider className="sider" theme={this.state.theme} onCollapse={this.onCollapse} collapsed={(this.state.width <= 1012) ? true : false}>
         <div className="userInfoContainer">
           <div className="userpic"></div>
           <h3 style={{color: (this.state.theme === 'dark' ? '#fafafa' : 'rgba(0, 0, 0, 0.65)')}}>Admin Panel</h3>
+
+          {(this.state.width > 1012) && (
+            <Row type="flex" justify="center">
+              <Col span={5}>
+                <Button onClick={this.changeTheme}
+                        className={this.state.theme === 'dark' ? 'btn-theme dark' : 'btn-theme light'}
+                        shape="circle"
+                        icon="bulb" />
+              </Col>
+              <Col span={5}>
+                <Button className={this.state.theme === 'dark' ? 'btn-theme dark' : 'btn-theme light'}
+                        shape="circle"
+                        icon="setting" />
+              </Col>
+              <Col span={5}>
+                <Button className={this.state.theme === 'dark' ? 'btn-theme dark' : 'btn-theme light'}
+                        shape="circle"
+                        icon="poweroff" />
+              </Col>
+            </Row>
+          )}
+
         </div>
         <Menu theme={this.state.theme} defaultSelectedKeys={['1']} mode="inline">
           <Menu.Item key="1">
@@ -89,14 +113,6 @@ export class Sidenav extends Component {
             <span>File</span>
           </Menu.Item>
         </Menu>
-        <div className="menuToolsContainer">
-          <Button onClick={this.changeTheme}
-                  className={this.state.theme === 'dark' ? 'btn-theme dark' : 'btn-theme light'}
-                  shape="circle"
-                  type="solid">
-            <Icon type="bulb" theme="filled"/>
-          </Button>
-        </div>
       </Sider>
     );
   }
